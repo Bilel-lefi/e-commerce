e403889e6efbddc1584be8f2eae830e421ec309d
moezaloui <moezaloui123@gmail.com>
2025-02-13 13:28:00 +0100

    change color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FaHome ,FaPhone, FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+import { FaHome, FaPhone, FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
 import { MdEmail } from "react-icons/md";
 
 const socialLinks = [
@@ -11,28 +11,28 @@ const socialLinks = [
 
 function Footer() {
   return (
-    <footer className="bg-emerald-950 text-white py-6 px-4">
+    <footer className="text-gray-500 group hover:bg-emerald-950 bg-white py-6 px-4">
       <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-6">
         {/* Informations de la société */}
-        <div className="mb-4">
-  <h2 className="text-2xl font-bold mb-3">Rj Deco</h2>
-  
-  <p className="flex items-center mb-2">
-    <FaHome className="mr-2" /> 123 Rue de l'Exemple, 75000 Paris
-  </p>
+        <div className="mb-4 group-hover:text-white">
+          <h2 className="text-2xl font-bold mb-3">Rj Deco</h2>
 
-  <p className="flex items-center mb-2">
-    <FaPhone className="mr-2" /> +216 45 678 098
-  </p>
+          <p className="flex items-center mb-2">
+            <FaHome className="mr-2" /> 123 Rue de l'Exemple, 75000 Paris
+          </p>
 
-  <p className="flex items-center">
-    <MdEmail className="mr-2" /> contactrejdeco@gmail.com
-  </p>
-</div>
+          <p className="flex items-center mb-2">
+            <FaPhone className="mr-2" /> +216 45 678 098
+          </p>
+
+          <p className="flex items-center">
+            <MdEmail className="mr-2" /> contactrejdeco@gmail.com
+          </p>
+        </div>
 
 
         {/* Réseaux sociaux */}
-        <div className="flex flex-col items-center mb-4">
+        <div className="flex flex-col items-center mb-4 group-hover:text-white">
           <h3 className="text-lg font-semibold mb-3">Suivez-nous</h3>
           <div className="flex space-x-6">
             {socialLinks.map(({ icon, href, color, label }) => (
@@ -52,13 +52,13 @@ function Footer() {
 
         {/* Newsletter */}
         <div className="flex flex-col items-center">
-          <h3 className="text-lg font-semibold mb-3">Abonnez-vous à notre newsletter</h3>
+          <h3 className="group-hover:text-white text-lg font-semibold mb-3">Abonnez-vous à notre newsletter</h3>
           <input
             type="email"
             placeholder="Entrez votre email"
-            className="px-4 py-2 text-black rounded-md mb-2 w-full"
+            className="px-4 py-2 text-black outline-none rounded-md mb-2 w-full"
           />
-          <button className="bg-white text-emerald-900 px-4 py-2 rounded-md hover:bg-gray-200">S'inscrire</button>
+          <button className="bg-white text-emerald-900 px-4 py-2 border border-gray-400 rounded-md hover:bg-gray-200">S'inscrire</button>
         </div>
       </div>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import a from '../assets/rj-deco-slider.jpg';
-
+import { motion } from 'framer-motion';
 function Home() {
   return (
     <section
